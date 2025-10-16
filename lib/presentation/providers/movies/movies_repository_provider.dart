1614d22be095f84b4d9010d9e821a798ebfaf161
Esperanza Cruz Galindo <dulcecrugal@gmail.com>
2025-10-16 13:55:04 -0600

    Add: Subiendo archivos restantes

--- lib/presentation/providers/movies/movies_repository_provider.dart
+++ lib/presentation/providers/movies/movies_repository_provider.dart
@@ -1,1 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia_220262/infrastructure/datasorces/moviedb_datasource.dart';
+import 'package:cinemapedia_220262/infrastructure/repositories/movie_repository_impl.dart';
+
+// Este repositorio es inmutable
+final movieRepositoryProvider = Provider((ref) {
+  return MovieRepositoryImpl(MoviedbDatasource());
+});
---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia_220262/presentation/providers/movies/movies_providers.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: _HomeView());
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+        return ListTile(title: Text(movie.title));
+      },
+    );
+  }
+}
